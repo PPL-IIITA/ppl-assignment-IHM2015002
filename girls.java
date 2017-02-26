d90e1175bed7aff3e0b3404b3cd2a29f95251ec6
IHM2015002 <ihm2015002@iiita.ac.in>
2017-02-26 18:45:35 +0530

    girls.java

---
+++ girls.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linux
+ */
+public class girls {
+    String name;
+ 	int maintenance;
+ 	int attar;
+ 	int expense;
+  	int intelligence;
+ 	int budget;
+ 	String boyf;
+  	String status;
+ 
+ 	
+ 
+public 	boolean is_eligible(int budget){
+ 	if (this.expense <= budget) {
+ 			return true;
+ 		}
+ 	else {
+ 			return false;
+ 		}
+ 	}
+ 	
+ }
+    
+
